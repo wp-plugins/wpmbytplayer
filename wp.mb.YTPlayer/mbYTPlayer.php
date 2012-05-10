246fe7b93eba866391ed59986bc3ab114674d41f
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-10 09:51:52 +0000

    changed name
    git-svn-id: https://plugins.svn.wordpress.org/wpmbytplayer/trunk@542292 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp.mb.YTPlayer/mbYTPlayer.php
+++ wp.mb.YTPlayer/mbYTPlayer.php
@@ -4,11 +4,11 @@ Plugin Name: mb.YTPlayer background video
 Plugin URI: http://pupunzi.com/#mb.components/mb.YTPlayer/YTPlayer.html
 Description: Play a Youtube video as background of your page. <strong>Go to settings > mbYTPlayer</strong> to activate the background video option for your homepage. Or use the shortcode following the reference in the settings panel. <strong>And don't forget to make a donation if you like it :-)</strong>
 Author: Pupunzi (Matteo Bicocchi)
-Version: 0.3
+Version: 0.4
 Author URI: http://pupunzi.com
 */
 
-define("MBYTPLAYER_VERSION", "0.3");
+define("MBYTPLAYER_VERSION", "0.4");
 
 register_activation_hook( __FILE__, 'mbYTPlayer_install' );
 
@@ -27,7 +27,7 @@ function isMobile() {
 
 // Still no luck? Let's have a look at the user agent on the browser. If it contains
 // any of the following, it's probably a mobile device. Kappow!
-    if(isset($_SERVER["HTTP_USER_AGENT"])){
+/*    if(isset($_SERVER["HTTP_USER_AGENT"])){
         $user_agents = array("midp", "j2me", "avantg", "docomo", "novarra", "palmos", "palmsource", "240x320", "opwv", "chtml", "pda", "windows ce", "mmp/", "blackberry", "mib/", "symbian", "wireless", "nokia", "hand", "mobi", "phone", "cdm", "up.b", "audio", "SIE-", "SEC-", "samsung", "HTC", "mot-", "mitsu", "sagem", "sony", "alcatel", "lg", "erics", "vx", "NEC", "philips", "mmm", "xx", "panasonic", "sharp", "wap", "sch", "rover", "pocket", "benq", "java", "pt", "pg", "vox", "amoi", "bird", "compal", "kg", "voda", "sany", "kdd", "dbt", "sendo", "sgh", "gradi", "jb", "dddi", "moto");
         foreach($user_agents as $user_string){
             if(preg_match("/".$user_string."/i",$_SERVER["HTTP_USER_AGENT"])) {
@@ -35,6 +35,7 @@ function isMobile() {
             }
         }
     }
+*/
 
 // Let's NOT return "mobile" if it's an iPhone, because the iPhone can render normal pages quite well.
     if(preg_match("/iphone/i",$_SERVER["HTTP_USER_AGENT"])) {
--- wp.mb.YTPlayer/readme.txt
+++ wp.mb.YTPlayer/readme.txt
@@ -4,7 +4,7 @@ Contributors: Pupunzi (Matteo Bicocchi)
 Tags: video player, Youtube, full background, video, flash, mov, jquery, pupunzi, mb.components
 Requires at least: 3.0
 Tested up to: 3.3.1
-Stable tag: 0.3
+Stable tag: 0.4
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DSHAHSJJCQ53Y
 
 Play any Youtube video as background of your page.
@@ -51,6 +51,11 @@ You should write the below shortcode into the content of your post or page:
 * @ opacity = a value from 0 to 1 that set the opacity of the background video
 
 == Changelog ==
+
+= 0.4 =
+
+* bugfix: Warning: preg_match() [function.preg-match]: Unknown modifier '/' in /home/content/28/9255928/html/wp-content/plugins/wpmbytplayer/mbYTPlayer.php on line 32.
+          removed the check.
 
 = 0.3 =
 
