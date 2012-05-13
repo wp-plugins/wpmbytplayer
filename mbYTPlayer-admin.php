99ebbc5f600fa8afddb660cf8921b8b300e60a0d
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-13 13:34:41 +0000

    v 0.5 Added: You can now add a short code to a text widget and it will be rendered as background video wherever the widget is printed out. Added: TinyMCE button to insert the short code.
    git-svn-id: https://plugins.svn.wordpress.org/wpmbytplayer/tags/0.5@543735 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mbYTPlayer-admin.php
@@ -0,0 +1,124 @@
+<?php
+
+// create the admin menu
+// hook in the action for the admin options page
+add_action('admin_menu', 'add_mbYTPlayer_option_page');
+
+function add_mbYTPlayer_option_page() {
+    // hook in the options page function
+    add_options_page('mbYTPlayer', 'mbYTPlayer', 'manage_options', __FILE__, 'mbYTPlayer_options_page');
+}
+function mbYTPlayer_options_page() { 	// Output the options page
+    global  $mbYTPlayer_version, $mbYTPlayer_home_video_url, $mbYTPlayer_show_controls, $mbYTPlayer_mute, $mbYTPlayer_ratio, $mbYTPlayer_loop, $mbYTPlayer_opacity  ?>
+<div class="wrap" style="width:800px">
+    <style>
+        .form-table th{
+            font-weight: bold!important;
+            border-bottom: 1px solid gray;
+        }
+        .form-table td{
+            border-bottom: 1px solid gray;
+        }
+        .submit{
+            text-align: right;
+        }
+
+    </style>
+
+    <form method="post" action="options.php">
+
+        <?php wp_nonce_field('update-options'); ?>
+
+
+        <h2>mb.YTPlayer Settings</h2>
+        <div class="updated fade">
+            <p style="line-height: 1.4em;">Thanks for downloading mb.YTPlayer! If you like it...<br />
+            </p>
+        </div>
+
+        <h2>Reference</h2>
+
+        <p>Leave the <b>home video url</b> blank if you don't want to display a background video on your homepage.
+        </p>
+        <p>You can add a mb.YTPlayer to any of your posts or pages by writing the shortcode
+            <br>
+            <b>[mbYTPlayer url="http://www.youtube.com/watch?v=V2rifmjZuKQ" ratio="4/3" mute="false" loop="true" showControls="true" opacity=1]</b>
+            into the content editor. Change the parameters into the shortcode to fill your needs.</p>
+        <br>
+        <h2>Home page background video properties:</h2>
+        <p>These settings are used only for the Home istance of the mb.YTPlayer component.</p>
+        <br>
+        <br>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">home video: url</th>
+                <td>
+                    <input type="text" name="mbYTPlayer_home_video_url" style="width:70%" value="<?php if (!empty($mbYTPlayer_home_video_url)) {echo $mbYTPlayer_home_video_url; }?>"/>
+                    <p>Copy and paste here the URL of the Youtube video you want as your homepage background.</p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">home video: opacity</th>
+                <td>
+                    <select name="mbYTPlayer_opacity">
+                        <option value=".3" <?php if ($mbYTPlayer_opacity==".3") {echo' selected'; }?> >0.3</option>
+                        <option value=".5" <?php if ($mbYTPlayer_opacity==".5") {echo' selected'; }?>>0.5</option>
+                        <option value=".8" <?php if ($mbYTPlayer_opacity==".8") {echo' selected'; }?>>0.8</option>
+                        <option value="1" <?php if ($mbYTPlayer_opacity=="1") {echo' selected'; }?>>1</option>
+                    </select>
+                    <p>Set the opacity of the background video.</p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">home video: aspect ratio</th>
+                <td>
+                    <select name="mbYTPlayer_ratio">
+                        <option value="4/3" <?php if ($mbYTPlayer_ratio=="4/3") {echo' selected'; }?> >4/3</option>
+                        <option value="16/9" <?php if ($mbYTPlayer_ratio=="16/9") {echo' selected'; }?>>16/9</option>
+                    </select>
+                    <p>Set the aspect-ratio of the background video.</p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">home video: show controls</th>
+                <td>
+                    <input type="checkbox" name="mbYTPlayer_show_controls" value="true" <?php if ($mbYTPlayer_show_controls=="true") {echo' checked="checked"'; }?>/>
+                    <p>Check to show controls at the bottom of the page.</p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">home video mute</th>
+                <td>
+                    <input type="checkbox" name="mbYTPlayer_mute" value="true" <?php if ($mbYTPlayer_mute=="true") {echo' checked="checked"'; }?>/>
+                    <p>Check to mute the audio of the video.</p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">home video loop</th>
+                <td>
+                    <input type="checkbox" name="mbYTPlayer_loop" value="true" <?php if ($mbYTPlayer_loop=="true") {echo' checked="checked"'; }?>/>
+                    <p>Check to loop the video once ended.</p>
+
+                </td>
+            </tr>
+        </table>
+
+        <input type="hidden" name="page_options" value="mbYTPlayer_home_video_url, mbYTPlayer_show_controls, mbYTPlayer_mute, mbYTPlayer_ratio, mbYTPlayer_loop, mbYTPlayer_opacity" />
+        <input type="hidden" name="action" value="update" />
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </p>
+        <a style="position: relative; display:block;top:0px;margin-right: -10px" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DSHAHSJJCQ53Y"><img border="0" alt="PayPal" src="<?php echo plugins_url( 'images/btn_donateCC_LG_global.gif', __FILE__ );?>" class="alignright"></a>
+    </form>
+
+    <p>You're using mb.YTPlayer v. <?php echo $mbYTPlayer_version;?> by <a href="http://pupunzi.com">Pupunzi</a>.<br>If you like it and you use it then you should consider a donation (€15,00 or more) :-)</p>
+    <a href="http://pupunzi.com"><img style="margin-top:30px;" src="<?php echo plugins_url( 'images/logo.png', __FILE__ );?>" alt="Made by Pupunzi" /></a>
+
+
+</div>
+<?php } ?>
