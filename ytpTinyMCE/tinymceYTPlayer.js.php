f3861a3a9c7e590e536c716d4e681fde13862985
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-19 20:32:46 +0000

    updated to v. 0.6.3: Added the link to Toutube video option both on settings and shortcode editor; if checked shows the link to the original video at the bottom right over the seek bar
    git-svn-id: https://plugins.svn.wordpress.org/wpmbytplayer/tags/0.6.3@614711 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ytpTinyMCE/tinymceYTPlayer.js.php
@@ -0,0 +1,39 @@
+<?php
+	if (!headers_sent()) {
+	    header("Content-Type: application/x-javascript; charset=UTF-8");
+	}
+?>
+(function() {
+
+    tinymce.create('tinymce.plugins.YTPlayer', {
+
+        init : function(ed, url) {
+
+        	var popUpURL = url + '/YTPlayertinymce.php?' + '<?php echo base64_decode(urldecode($_GET['params'])); ?>';
+
+			ed.addCommand('YTPlayerpopup', function() {
+				ed.windowManager.open({
+					url : popUpURL,
+					width : 600,
+					height : 450,
+					inline : 1
+				});
+			});
+
+			ed.addButton('YTPlayerbutton', {
+				title : 'YTPlayer TinyMCE',
+				image : url + '/YTPlayerbutton.png',
+				cmd : 'YTPlayerpopup'
+			});
+		},
+
+		createControl : function() {
+            return null;
+        },
+
+		getInfo : function() {
+            return {};
+        }
+    });
+    tinymce.PluginManager.add('YTPlayer', tinymce.plugins.YTPlayer);
+}());
