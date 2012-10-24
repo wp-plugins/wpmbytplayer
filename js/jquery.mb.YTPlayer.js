57e64352819831e34c5910c6175d57a5ded9fba6
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-24 16:39:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpmbytplayer/tags/0.6.1@616923 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery.mb.YTPlayer.js
+++ js/jquery.mb.YTPlayer.js
@@ -18,10 +18,10 @@
  */
 
 /*Browser detection patch*/
-jQuery.browser.mozilla = /firefox/.test(navigator.userAgent.toLowerCase());
-jQuery.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
-jQuery.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
-jQuery.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
+$.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase()) ;
+$.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
+$.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
+$.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
 
 (function(jQuery){
 
