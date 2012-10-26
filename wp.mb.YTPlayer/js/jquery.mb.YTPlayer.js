bab7a8cfa82403c1dec393b52c6b6c370ecbed39
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-26 20:42:02 +0000

    update to v. 0.3 --- Many bugfix on the TinyMCE editor plugin
    git-svn-id: https://plugins.svn.wordpress.org/wpmbytplayer/trunk@617857 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp.mb.YTPlayer/js/jquery.mb.YTPlayer.js
+++ wp.mb.YTPlayer/js/jquery.mb.YTPlayer.js
@@ -19,10 +19,10 @@
 
 /*Browser detection patch*/
 /*Browser detection patch*/
-$.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase()) ;
-$.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
-$.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
-$.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase()) ;
+jQuery.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
 
 (function(jQuery){
 
