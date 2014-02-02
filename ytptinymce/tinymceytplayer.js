cf06f4ae215da08b86b11c0e6bb7efb40fce431c
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-02 15:52:48 +0000

    = 1.7.5 = * fixed bug on the shortcode editor window where the "autoplay" option where shown only if "is inline" was checked. * Added the "stopAt" option to set when the video should stop.
    git-svn-id: https://plugins.svn.wordpress.org/wpmbytplayer/tags/1.7.5@849975 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ytptinymce/tinymceytplayer.js
@@ -0,0 +1,35 @@
+(function() {
+
+    window.tinyPopUp = {};
+
+      tinymce.create('tinymce.plugins.YTPlayer', {
+
+        init : function(ed, url) {
+        	var popUpURL = url + '/ytplayertinymce.php';
+
+			ed.addCommand('YTPlayerpopup', function() {
+				ed.windowManager.open({
+					url : popUpURL,
+					width : 600,
+					height : 670,
+					inline : 1
+				});
+			});
+
+			ed.addButton('YTPlayerbutton', {
+				title : 'YTPlayer TinyMCE',
+				image : url + '/ytplayerbutton.png',
+				cmd : 'YTPlayerpopup'
+			});
+		},
+
+		createControl : function() {
+            return null;
+        },
+
+		getInfo : function() {
+            return {};
+        }
+    });
+    tinymce.PluginManager.add('YTPlayer', tinymce.plugins.YTPlayer);
+}());
